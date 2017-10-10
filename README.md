f6201c9644a5a6214a204f5f16b0c42712352102
Matt Hoyle <code@deployable.co>
2017-10-10 06:54:37 +0000

    Multiple builds for https://hub.docker.com/r/deployable/unbound/
    https://hub.docker.com/r/deployable/unbound/

---
+++ README.md
@@ -0,0 +1,9 @@
+# Unbound in Docker
+
+Unbound forwarders in a container - https://hub.docker.com/r/deployable/unbound/
+
+## Local
+
+./make.sh build
+
+
--- make.sh
+++ make.sh
@@ -7,16 +7,66 @@ IMG_NAME=unbound
 IMG_TAG=$IMG_NAMESPACE/$IMG_NAME
 CONTAINER_NAME=unbound-forward
 
-sudo docker build -t $IMG_TAG .
-set +e
-sudo docker rm -f $CONTAINER_NAME
-set -e
-sudo docker run \
-  --detach \
-  --name $CONTAINER_NAME \
-  --publish 53:53/udp \
-  --restart always \
-  $IMG_TAG
+rundir=$(cd -P -- "$(dirname -- "$0")" && printf '%s\n' "$(pwd -P)")
+canonical="$rundir/$(basename -- "$0")"
 
-sudo docker ps 
-sudo docker logs -f $CONTAINER_NAME
+if [ -n "${1:-}" ]; then
+  cmd=$1
+  shift
+else
+  cmd=build
+fi
+
+cd "$rundir"
+
+
+build(){
+  build_default
+  build_apse2 
+}
+
+build_default(){
+  template unbound.conf Dockerfile
+  docker build -t $IMG_TAG .
+}
+
+build_apse2(){
+  template unbound.aws.apse2.conf Dockerfile.apse2
+  docker build -t $IMG_TAG-apse2 -f Dockerfile.apse2 .
+}
+
+run(){
+  set +e
+  docker rm -f $CONTAINER_NAME
+  set -e
+  docker run \
+    --detach \
+    --name $CONTAINER_NAME \
+    --publish 53:53/udp \
+    --restart always \
+    $IMG_TAG
+
+  docker ps
+  docker logs --tail 20 $CONTAINER_NAME
+}
+
+template(){
+  template_conffile=$1
+  template_dockerfile=$2
+  perl -pe 'BEGIN{ $conffile=shift @ARGV; } s/{{unbound_conf}}/$conffile/' $template_conffile Dockerfile.template > "$template_dockerfile"
+}
+
+run_help(){
+  echo "Commands:"
+  awk '/  ".*"/{ print "  "substr($1,2,length($1)-3) }' make.sh
+}
+
+set +x
+
+case $cmd in
+  "build")     build "$@";;
+  "template")  template "$@";;
+  "run")       run "$@";;
+  '-h'|'--help'|'h'|'help') run_help;;
+esac
+
